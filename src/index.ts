4c301935a94d6a501156bcf796141829ac815cc6
tvl83 <tvle83@gmail.com>
2022-04-15 17:02:05 -0500

    update to point to testnet added github workflow actions

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,28 @@
 const axios    = require('axios').default;
-var bodyParser = require('body-parser')
+let bodyParser = require('body-parser');
+let cors       = require('cors')
 let express    = require('express');
 let app        = express();
 
 let port = 3033;
-app.use(bodyParser.json())
+
+app.use(cors());
+app.options('*', cors())
+app.use(bodyParser.json());
+
 app.listen(port, () => {
 	console.log(`FIO Marketplace Middleware API listening on http://localhost:${port}`)
 })
-// https://api.orbitalcommand.io/supply.html
+
 app.use(express.static('public'))
 
+// https://api.fiomarket.place/get_escrow_listing
 app.post('/get_escrow_listings', async (req: any, res: any) => {
 	try {
 		let reqBody = req.body;
 		console.log(reqBody)
 
-		let result = await axios.post(`https://fio-devnet.eosblocksmith.io/v1/chain/get_escrow_listings`, {
+		let result = await axios.post(`https://fio-testnet.eosblocksmith.io/v1/chain/get_escrow_listings`, {
 			"status": reqBody.status,
 			"offset": reqBody.offset,
 			"limit" : reqBody.limit,
