560829fe014bbeee8b9f4157cf4470d1b7f6e1f6
tvl83 <tvle83@gmail.com>
2022-04-18 16:21:32 -0500

    use environment variables for settings.

--- src/index.ts
+++ src/index.ts
@@ -4,25 +4,23 @@ let cors       = require('cors')
 let express    = require('express');
 let app        = express();
 
-let port = 3033;
+const PORT = process.env.PORT === undefined ? 3033 : process.env.PORT;
+const API_HOST = process.env.API_HOST === undefined ? `https://fio-testnet.eosblocksmith.io` : process.env.API_HOST;
 
 app.use(cors());
 app.options('*', cors())
 app.use(bodyParser.json());
 
-app.listen(port, () => {
-	console.log(`FIO Marketplace Middleware API listening on http://localhost:${port}`)
+app.listen(PORT, () => {
+	console.log(`FIO Marketplace Middleware API listening on http://localhost:${PORT}`)
 })
 
 app.use(express.static('public'))
 
-// https://api.fiomarket.place/get_escrow_listing
 app.post('/get_escrow_listings', async (req: any, res: any) => {
 	try {
 		let reqBody = req.body;
-		console.log(reqBody)
-
-		let result = await axios.post(`https://fio-testnet.eosblocksmith.io/v1/chain/get_escrow_listings`, {
+		let result = await axios.post(`${API_HOST}/v1/chain/get_escrow_listings`, {
 			"status": reqBody.status,
 			"offset": reqBody.offset,
 			"limit" : reqBody.limit,
